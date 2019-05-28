c2f222e3764b3995c0cb6c838c534d3283531fb2
l8656 <865692894@qq.com>
2019-05-28 21:29:12 +0800

    修改了user

--- spring-redis/spring-redis/src/main/java/User.java
+++ spring-redis/spring-redis/src/main/java/User.java
@@ -5,6 +5,7 @@ public class User {
     private String name;
     private int age;
     private String addr;
+    private String city;
 
     public void say(){
         System.out.println("hello you");
